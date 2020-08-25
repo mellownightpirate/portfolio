9125cc0bc3418674f2e95db9867e09c82f771726
mellownightpirate <a@hasan.co>
2020-08-25 17:02:06 +0100

    starting work section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import typewriter from "./typewriter";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <header>
+          <h1>
+            AMIN<span style={headingStyle}>.</span>
+          </h1>
+          <nav>
+            <a href="">Work</a>
+            <a href="">Me</a>
+            <a href="">Contact</a>
+          </nav>
+        </header>
+        <div className="container py-5">
+          <div className="output" id="output">
+            <h2 className="cursor"></h2>
+            <br />
+            <h3></h3>
+          </div>
+          <br />
+          <div>
+            <i className="arrow down"></i>
+          </div>
+        </div>
+        
+      </div>
+      <div className="work"></div>
     </div>
   );
 }
+
+var headingStyle = {
+  color: "RGB(146, 139, 97)",
+};
 
 export default App;
